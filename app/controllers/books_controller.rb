4ff59b062bc5a4963ef0e555d6262dc15f12e75d
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-03-26 20:58:10 +0900

    不要なコードを削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,8 +65,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      p "ピカチュウ"
-      p params
       params.require(:book).permit(:title, :memo, :author, :picture)
     end
 end
